52b6fa72ab59d31317d064de75147800308fa0e9
lispy-dobby <yilkalargawworkneh@gmail.com>
2018-10-26 05:50:10 +0300

    added validations to some of the functions
    the validated functions are
        . initialize
        . preprocess
        . convert_2digit
        . convert_each_2_digit
        . rollback
    
    the validation functions are
        . validinput?
        . valid_for_2digit?
        . valid_array_for_each_2digit?

--- arabify.rb
+++ arabify.rb
@@ -4,13 +4,20 @@
 # This class contains the processing tools to convert  Geeze numbers to arabic.
 class Arabify
 
-  @@numhash = Hash['፩' => 1, '፪' => 2, '፫' => 3, '፬' => 4,
-                   '፭' => 5, '፮' => 6, '፯' => 7, '፰' => 8,
-                   '፱' => 9, '፲' => 10, '፳' => 20, '፴' => 30,
-                   '፵' => 40, '፶' => 50, '፷' => 60, '፸' => 70,
-                   '፹' => 80, '፺' => 90, ' ' => 0]
+  ERROR_MSG_CONSTRUCTOR = 'invalid input the string is not a geez number'.freeze
+  ERROR_MSG1 = 'invalid input to method convert_2digit'.freeze
+  ERROR_MSG2 = 'invalid input to method preprocess'.freeze
+  ERROR_MSG3 = 'invalid input to method rollback'.freeze
+  ERROR_MSG3 = 'invalid input to method convert_each_2_digit'.freeze
+  NUMHASH = Hash['፩' => 1,  '፪' => 2,  '፫' => 3,  '፬' => 4,
+                 '፭' => 5,  '፮' => 6,  '፯' => 7,  '፰' => 8,
+                 '፱' => 9,  '፲' => 10, '፳' => 20, '፴' => 30,
+                 '፵' => 40, '፶' => 50, '፷' => 60, '፸' => 70,
+                 '፹' => 80, '፺' => 90, ' ' => 0]
 
   def initialize(str)
+    raise ArgumentError, ERROR_MSG_CONSTRUCTOR unless validinput?(str)
+
     @geezstrstr = str
   end
 
@@ -26,11 +33,15 @@ class Arabify
   private
 
   def preprocess(str)
+    raise ArgumentError, ERROR_MSG2 unless validinput?(str)
+
     split_by_100_and_padd(split_by_10000s(rollback(str)))
   end
 
   def convert_2digit(str)
-    str.split('').sum { |x| @@numhash[x] }
+    raise ArgumentError, ERROR_MSG1 unless valid_for_2digit?(str)
+
+    str.split('').sum { |x| NUMHASH[x] }
   end
 
   def split_by_10000s(str)
@@ -44,10 +55,14 @@ class Arabify
   end
 
   def convert_each_2_digit(arr)
+    raise ArgumentError, ERROR_MSG3 unless valid_array_for_each_2digit?(arr)
+
     arr.map { |y| convert_2digit(y) }
   end
 
   def rollback(str)
+    raise ArgumentError, ERROR_MSG3 unless validinput?(str)
+
     separate_10000(
       str
         .gsub('፼፻', '፼፩፻')
@@ -59,5 +74,24 @@ class Arabify
   def separate_10000(str)
     str.gsub!('፼፼', '፼ ፼') while str.include?('፼፼')
     str
+  end
+
+  def validinput?(str)
+    str.split('')
+       .map { |x| NUMHASH.key?(x) || x == '፼' || x == '፻' }
+       .reduce(true) { |result, n| result && n }
+  end
+
+  def valid_for_2digit?(str)
+    !str.empty? &&
+      str.length <= 2 &&
+      str.split('')
+         .map { |x| NUMHASH.key?(x) }
+         .reduce(true) { |res, n| res && n }
+  end
+
+  def valid_array_for_each_2digit?(arr)
+    arr.map { |x| valid_for_2digit?(x) }
+       .reduce(true) { |res, n| res && n }
   end
 end
