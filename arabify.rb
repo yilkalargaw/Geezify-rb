5df03342ca3ba7cd4059bd0bd7326d65574dbeb2
lispy-dobby <yilkalargawworkneh@gmail.com>
2018-11-01 10:39:09 +0300

    replaced the whole conversion method
    the conversion method is changed to a more simpler one for speed, readability.

--- arabify.rb
+++ arabify.rb
@@ -6,9 +6,6 @@ class Arabify
 
   ERROR_MSG_CONSTRUCTOR = 'invalid input the string is not a geez number'.freeze
   ERROR_MSG1 = 'invalid input to method convert_2digit'.freeze
-  ERROR_MSG2 = 'invalid input to method preprocess'.freeze
-  ERROR_MSG3 = 'invalid input to method rollback'.freeze
-  ERROR_MSG3 = 'invalid input to method convert_each_2_digit'.freeze
   NUMHASH = Hash['፩' => 1,  '፪' => 2,  '፫' => 3,  '፬' => 4,
                  '፭' => 5,  '፮' => 6,  '፯' => 7,  '፰' => 8,
                  '፱' => 9,  '፲' => 10, '፳' => 20, '፴' => 30,
@@ -18,62 +15,45 @@ class Arabify
   def initialize(str)
     raise ArgumentError, ERROR_MSG_CONSTRUCTOR unless validinput?(str)
 
-    @geezstrstr = str
+    @geezstr = str
   end
 
   def arabify
-    preprocess(@geezstrstr)
-      .map { |x| x[0] * 100 + x[1] }
-      .reverse
+    preprocessed = rollback(@geezstr.gsub('፼', '፼ ')).split('፼')
+
+    preprocessed
       .each_with_index
-      .map { |x, i| x * (10_000**i) }
-      .sum
+      .reduce(0) do |sum, (v, i)|
+      sum + convert_upto10000(v.strip) * (10_000**(preprocessed.length - 1 - i))
+    end
   end
 
   private
 
-  def preprocess(str)
-    raise ArgumentError, ERROR_MSG2 unless validinput?(str)
-
-    split_by_100_and_padd(split_by_10000s(rollback(str)))
-  end
-
-  def convert_2digit(str)
-    raise ArgumentError, ERROR_MSG1 unless valid_for_2digit?(str)
-
-    str.split('').sum { |x| NUMHASH[x] }
-  end
-
-  def split_by_10000s(str)
-    str.gsub(/፼$/, '፼ ').split('፼')
-  end
-
-  def split_by_100_and_padd(str)
-    str.map { |x| x[-1] == '፻' ? x << ' ' : x }
-       .map { |x| convert_each_2_digit(x.split('፻')) }
-       .each { |x| x.length == 1 && x.prepend(0) }
-  end
-
-  def convert_each_2_digit(arr)
-    raise ArgumentError, ERROR_MSG3 unless valid_array_for_each_2digit?(arr)
-
-    arr.map { |y| convert_2digit(y) }
-  end
-
   def rollback(str)
     raise ArgumentError, ERROR_MSG3 unless validinput?(str)
 
-    separate_10000(
-      str
-        .gsub('፼፻', '፼፩፻')
-        .gsub(/^፻/, '፩፻')
-        .gsub(/^፼/, '፩፼')
-    )
+    str.gsub('፼፻', '፼፩፻').gsub(/^፻/, '፩፻').gsub(/^፼/, '፩፼')
   end
 
-  def separate_10000(str)
-    str.gsub!('፼፼', '፼ ፼') while str.include?('፼፼')
-    str
+  def convert_2digit(string)
+    str = string || ''
+    raise ArgumentError, ERROR_MSG1 unless valid_for_2digit?(str)
+
+    str.split('').sum { |x| x == '' ? 0 : NUMHASH[x] }
+  end
+
+  def convert_upto10000(str)
+    return unless valid_for_convupto10000?(str)
+
+    pos100 = str.index('፻')
+
+    return convert_2digit(str) if pos100.nil?
+
+    return 100 + convert_2digit(str[1..-1]) if pos100.zero?
+
+    convert_2digit(str[0..(pos100 - 1)]) * 100 +
+      convert_2digit(str[(pos100 + 1)..-1])
   end
 
   def validinput?(str)
@@ -83,15 +63,12 @@ class Arabify
   end
 
   def valid_for_2digit?(str)
-    !str.empty? &&
-      str.length <= 2 &&
-      str.split('')
-         .map { |x| NUMHASH.key?(x) }
-         .reduce(true) { |res, n| res && n }
+    str.length <= 2 && str.split('')
+                          .map { |x| NUMHASH.key?(x) }
+                          .reduce(true) { |res, n| res && n }
   end
 
-  def valid_array_for_each_2digit?(arr)
-    arr.map { |x| valid_for_2digit?(x) }
-       .reduce(true) { |res, n| res && n }
+  def valid_for_convupto10000?(str)
+    str.is_a?(String) && str.length <= 5 && str.match('፼').nil?
   end
 end
