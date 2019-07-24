d7e331dfff14cdd6eff8f0c95bae4e809ac8582f
yilkalargaw <yilkalargawworkneh@gmail.com>
2019-07-24 11:39:18 +0300

    added gemspec , version and Rake files necessary for gemification

--- lib/geezify-rb.rb
+++ lib/geezify-rb.rb
@@ -1,5 +1,6 @@
 require_relative './geezify-rb/geezify.rb'
 require_relative './geezify-rb/arabify.rb'
+require_relative './geezify-rb/version.rb'
 
 module GeezifyRb
 end
---
+++ lib/geezify-rb/version.rb
@@ -0,0 +1,3 @@
+module GeezifyRb
+    VERSION = "0.2.0"
+end
