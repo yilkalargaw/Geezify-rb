131167841ab7624130f355a97a6e03a3876b00b9
yilkalargaw <yilkalargawworkneh@gmail.com>
2020-07-30 12:01:12 +0300

    * test/both_backward_test.rb: moved the backwards test to another file

---
+++ test/both_backward_test.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+require 'minitest/autorun'
+require_relative '../lib/geezify-rb.rb'
+require_relative 'test_case'
+
+class GeezifyTest < Minitest::Test
+  include GeezifyRb
+
+  def test_geezify_then_arabify
+    TESTDATA.map do |x|
+      assert_equal x[0],
+        GeezifyRb::Arabify.arabify(GeezifyRb::Geezify.geezify(x[0]))
+    end
+  end
+
+  def test_arabify_then_geezify
+    TESTDATA.map do |x|
+      assert_equal x[1],
+        GeezifyRb::Geezify.geezify(GeezifyRb::Arabify.arabify(x[1]))
+    end
+  end
+end
